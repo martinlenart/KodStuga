65e9ecc687a37adc8a562ae4125e523dde8c3775
Martin Lenart <martin_lenart@icloud.com>
2022-12-01 16:41:03 +0100

    Add project files.

---
+++ KodStuga1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace KodStuga1
+{
+    public enum GrapeVariants
+    {
+        CabernetSauvignon, PinotNoir, Corvina, Shiraz, Merlot, Chablis,
+        Riesling, Tempranillo
+    }
+    public enum GrapeRegions
+    {
+        Bordeaux, Burgundy, Veneto, Piedmonte, RiberaDelDuero,
+        NapaValley, Puglia, Pfalz
+    }
+    public struct Wine
+    {
+        public int? Year;                   // null = undefined
+        public string Name;
+        public GrapeVariants Grape;
+        public GrapeRegions Region;
+
+        /// <summary>
+        /// Creates a string representing the content of the Wine struct
+        /// </summary>
+        /// <returns>string that can be printed out using Console.WriteLine</returns>
+        public override string ToString()
+        {
+            //Your code
+            return $"";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int maxNrBottles = 4;
+            Wine[] myCellar = new Wine[maxNrBottles];
+
+            Console.WriteLine($"My cellar can have maximum {maxNrBottles} bottles");
+
+            Wine wine1 = new Wine { Year = 2000, Name = "Château Lafite Rothschild", Grape = GrapeVariants.CabernetSauvignon, Region = GrapeRegions.Bordeaux };
+            bool bOK = InsertWine(myCellar, wine1);
+
+            Wine wine2 = new Wine { Year = 2010, Name = "Domaine de la Romanée-Conti", Grape = GrapeVariants.PinotNoir, Region = GrapeRegions.Burgundy };
+            bOK = InsertWine(myCellar, wine2);
+
+            Wine wine3 = new Wine { Year = 2005, Name = "Giuseppe Quintarelli Amarone", Grape = GrapeVariants.Corvina, Region = GrapeRegions.Veneto };
+            bOK = InsertWine(myCellar, wine3);
+
+            Wine wine4 = new Wine { Year = 2008, Name = "Sierra Cantabria", Grape = GrapeVariants.Tempranillo, Region = GrapeRegions.RiberaDelDuero };
+            bOK = InsertWine(myCellar, wine3);
+
+            Wine wine5 = new Wine { Year = 1992, Name = "Screaming Eagle", Grape = GrapeVariants.CabernetSauvignon, Region = GrapeRegions.RiberaDelDuero };
+            bOK = InsertWine(myCellar, wine3);
+
+            PrintWines(myCellar);
+
+            Console.WriteLine(NrOfBottles(myCellar));
+            Console.WriteLine(NrOfBottles(myCellar, GrapeVariants.CabernetSauvignon));
+        }
+
+        /// <summary>
+        /// Inserts a wine into myCellar at first available position
+        /// </summary>
+        /// <param name="myCellar"></param>
+        /// <param name="wine"></param>
+        /// <returns>true - if insertion was possible, otherwise false</returns>
+        private static bool InsertWine(Wine[] myCellar, Wine wine)
+        {
+            //Your code
+        }
+
+        /// <summary>
+        /// Print out all the wines in myCellar
+        /// </summary>
+        /// <param name="myCellar"></param>
+        private static void PrintWines(Wine[] myCellar)
+        {
+            //Your code
+        }
+
+        /// <summary>
+        /// Counts the number of bottles in myCellar. 
+        /// </summary>
+        /// <param name="myCellar"></param>
+        /// <returns>Number of bottles in myCellar</returns>
+        private static int NrOfBottles(Wine[] myCellar)
+        {
+            //Your code
+        }
+
+        /// <summary>
+        /// Counts the number of bottles in myCellar with a certain GrapeVariant 
+        /// </summary>
+        /// <param name="myCellar"></param>
+        /// <param name="grape"></param>
+        /// <returns></returns>
+        private static int NrOfBottles(Wine[] myCellar, GrapeVariants grape)
+        {
+            //Your code
+        }
+
+    }
+}
